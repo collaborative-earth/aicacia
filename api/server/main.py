4d8fa3793ae370026ed43b8724b90e60453451d4
Guru <guru@coefficient.io>
2024-08-30 07:30:10 -0700

    Adding cors

--- api/server/main.py
+++ api/server/main.py
@@ -3,10 +3,19 @@ from controllers.hello_world_controller import hello_router
 from controllers.user_controller import user_router
 from controllers.user_query_controller import user_query_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app():
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     app.include_router(hello_router, prefix="/hello", tags=["hello"])
     app.include_router(user_query_router, prefix="/user_query", tags=["user_query"])
