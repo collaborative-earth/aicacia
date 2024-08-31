3b3ce879849ae26cd8b5b83a13a8c4b52018f2cb
Guru <guru@coefficient.io>
2024-08-31 00:04:17 -0700

    moving the logout to the right

--- webapp/src/components/TopBar.tsx
+++ webapp/src/components/TopBar.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import logo from '../assets/aicacia_logo.png';
+import { getToken, removeToken } from '../utils/tokens';
 
 const TopBar: React.FC = () => {
+  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleButtonClick = () => {
+    removeToken();
+    navigate('/login');
+  };
+
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      setLoggedIn(true);
+    }
+  }, []);
+
   return (
     <header className="top-bar">
-      <Link to="/" className="top-bar-link">
-        <img src={logo} alt="Aicacia Logo" className="logo" />
-        <h1>Aicacia</h1>
-      </Link>
+      <div className="top-bar-left">
+        <Link to="/" className="top-bar-link">
+          <img src={logo} alt="Aicacia Logo" className="logo" />
+          <h1>Aicacia</h1>
+        </Link>
+      </div>
+      {loggedIn && (
+        <div className="top-bar-right">
+          <button onClick={handleButtonClick}>Logout</button>
+        </div>
+      )}
     </header>
   );
 };
