b2ade54b434ee58a3f993b666f38d615736fbf51
Rahul Varma <rahulsvarm@gmail.com>
2024-10-18 07:38:32 -0700

    Initial commit for qdrant vectordb

--- vectordb/main.py
+++ vectordb/main.py
@@ -1,80 +1,80 @@
 import argparse
 import os
-import sys
 from glob import glob
 
-from llama_index.core import SimpleDirectoryReader
-from llama_index.core.ingestion import IngestionPipeline
-from llama_index.core.node_parser import HTMLNodeParser, MarkdownNodeParser
+import ollama
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, PointStruct, VectorParams
 from tqdm import tqdm
+
 from utils import *
 
-sys.path.append("..")
-from finetuning.src.node_parsers.tei_parser import TEINodeParser
+
+def extract_text_from_pdf(pdf_path):
+    full_text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            full_text += page.extract_text()
+    return full_text
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="VectorDB Arguments")
     parser.add_argument('-c', '--collection', help="Collection Name")
-    parser.add_argument('-d', '--input-dir', help="Input Directory")
+    parser.add_argument('-m', '--embedding-model', default="mxbai-embed-large", help="Embedding model")
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
+    # Dictionary mapping file extensions to their respective functions
     extraction_functions = {
-            '.md': MarkdownNodeParser(),
-            '.html': HTMLNodeParser(),
-            '.pdf': MarkdownNodeParser(),
-            '.xml': TEINodeParser()
-        }
-        
+        '.html': extract_text_from_html,
+        '.pdf': extract_text_from_pdf,
+    }
+
     args = parse_args()
     collection_name = args.collection
-    input_dir = args.input_dir
+    file_paths = glob('../data/**/*', recursive=True)
+    file_paths = [file_path for file_path in file_paths if os.path.isfile(file_path)]
 
-    config = load_config('config.yaml')
-    text_splitter = create_text_splitter(config)
-    embed_model = create_embedding_class(config)
-    vector_store = create_vectordb_client(config, collection_name)
-    metadata = read_db(input_dir)[['id', 'title', 'doi', 'authors',
-                                   'corpus_name', 'sources', 'location', 
-                                   'sourced_date', 'revision_date',
-                                   'provided_tags', 'generated_tags']]
-
-    for ext, file_parser in extraction_functions.items():
-        try:
-            reader = SimpleDirectoryReader(input_dir=input_dir,
-                                           recursive=True,
-                                           required_exts=[ext],
-                                           file_extractor = create_file_extractor(config),
-                                           num_files_limit=100)
-        except Exception as e:
-            if 'No files found' in str(e):
-                continue
-            else:
-                raise Exception(e)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=512,
+        chunk_overlap=50,
+        length_function=len,
+        separators=["\n\n", "\n", " ", ""]
+    )
+    client = QdrantClient("localhost", port=6333)
         
-        docs = []
-        input_files = reader.input_files
-        for file_path in tqdm(input_files):
-            reader.input_files = [file_path]
-            file_name = str(file_path).split('/')[-1].split('.')[-2]
-            try:
-                file_metadata = metadata[metadata['id'] == file_name].iloc[0].to_dict()
-            except:
-                file_metadata = {}
+    delete_collection_if_exists(client, collection_name)
+    client.create_collection(
+        collection_name=collection_name,
+        vectors_config=VectorParams(size=1024, distance=Distance.COSINE),
+    )
+    unique_id = 0
 
-            # Load the document with the file extractor
-            loaded_docs = reader.load_data()
-            
-            # Enrich each document with metadata
-            for doc in loaded_docs:
-                doc.metadata.update(file_metadata)
-                doc.excluded_llm_metadata_keys = list(doc.metadata.keys())
-                doc.excluded_embed_metadata_keys = list(doc.metadata.keys())
-            docs.extend(loaded_docs)
+    for doc_id, file_path in tqdm(enumerate(file_paths), total=len(file_paths)):        
+        # Call the appropriate function based on the extension
+        file_name = file_path.split('/')[-1]
+        try:
+            text = extraction_functions[get_file_extension(file_path).lower()](file_path)
+        except KeyError:
+            raise ValueError(f"Unsupported file type: {extension}")
 
+        chunks = text_splitter.split_text(text)
+        points = []
 
-        pipeline = IngestionPipeline(transformations=[file_parser, text_splitter, MetadataCleanupTransformation(), embed_model], vector_store=vector_store)
-        nodes = pipeline.run(documents=docs, show_progress=True)
+        for chunk_id, chunk in enumerate(chunks):
+            response = ollama.embeddings(
+                model=args.embedding_model,
+                prompt=chunk,
+            )
+            chunk_embedding = response["embedding"]
+
+            metadata = {"doc_id": doc_id, "chunk_id": chunk_id, "doc_name": file_name, "text": chunk}
+            client.upsert(collection_name=collection_name,
+                                 points=[PointStruct(id=unique_id, payload=metadata, vector=chunk_embedding)])
+            unique_id += 1
+
+        client.upload_points(collection_name, points)
--- vectordb/utils.py
+++ vectordb/utils.py
@@ -1,31 +1,30 @@
 import os
-import sqlite3
-from glob import glob
-from typing import Dict, List
 
-import pandas as pd
-import yaml
-from llama_index.core.node_parser import SentenceSplitter
-from llama_index.core.schema import BaseNode, TransformComponent
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.readers.file import FlatReader, HTMLTagReader, PDFReader
-from llama_index.vector_stores.qdrant import QdrantVectorStore
-from qdrant_client import QdrantClient
+import pdfplumber
+from bs4 import BeautifulSoup
 
 
-class MetadataCleanupTransformation(TransformComponent):
-    """Transformation to remove unwanted metadata fields."""
-    def __init__(self):
-        super().__init__()
+def extract_text_from_pdf(path):
+    full_text = ""
+    with pdfplumber.open(path) as pdf:
+        for page in pdf.pages:
+            full_text += page.extract_text()
+    return full_text
 
-    def __call__(self, nodes: List[BaseNode], **kwargs) -> List[BaseNode]:
-        for node in nodes:
-            # Remove any unwanted metadata keys"
-            node.metadata = {k: v for k, v in node.metadata.items() 
-                           if not k.startswith("Header_")
-                           and k != 'filename'
-                           and k != 'file_path'}
-        return nodes
+
+def get_file_extension(file_path):
+    _, extension = os.path.splitext(file_path)
+    return extension
+
+    
+def extract_text_from_html(path):
+    full_text = ""
+    with open(path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # Extract text from all tags
+        full_text = soup.get_text()
+    return full_text
 
 
 def delete_collection_if_exists(client, collection_name):
@@ -35,78 +34,3 @@ def delete_collection_if_exists(client, collection_name):
         print(f"Collection '{collection_name}' has been deleted.")
     else:
         print(f"Collection '{collection_name}' does not exist.")
-
-
-def load_config(file_path):
-    with open(file_path, 'r') as file:
-        return yaml.safe_load(file)
-
-
-def create_text_splitter(config):
-    splitter_config = config.get('text-splitter', {})
-    if splitter_config is None or len(splitter_config) != 1:
-        raise Exception('Define one text splitter function!')
-    splitter_class = next(iter(splitter_config))
-    params = splitter_config[splitter_class]
-
-    try:
-        return globals()[splitter_class](**params)
-    except:
-        raise ValueError(f"Unsupported text splitter: {splitter_class}")
-
-
-def create_vectordb_client(config, collection_name):
-    vectordb_mapping = {
-        'QdrantClient': QdrantVectorStore,
-    }
-
-    vectordb_config = config.get('vectordb', {})
-    if vectordb_config is None or len(vectordb_config) != 1:
-        raise Exception('Define one vectordb function!')
-    vectordb_class = next(iter(vectordb_config))
-    params = vectordb_config[vectordb_class]
-
-    try:
-        client = globals()[vectordb_class](**params)
-        delete_collection_if_exists(client, collection_name=collection_name)
-        return vectordb_mapping[type(client).__name__](client=client, collection_name=collection_name)
-    except:
-        raise ValueError(f"Unsupported vectordb client: {vectordb_class}")
-
-
-def create_embedding_class(config):
-    embedding_config = config.get('embedding', {})
-    if embedding_config is None or len(embedding_config) != 1:
-        raise Exception('Define one embedding function!')
-    embedding_class = next(iter(embedding_config))
-    params = embedding_config[embedding_class]
-
-    try:
-        return globals()[embedding_class](**params)
-    except:
-        raise ValueError(f"Unsupported embedding class: {embedding_class}")
-
-
-def create_file_extractor(config):
-    file_extractor_config = config.get('file_extractor', {})
-    extractors = {}
-
-    for file_type, file_type_class in file_extractor_config.items():
-        try:
-            extractor_class = getattr(__import__('llama_index.readers.file', fromlist=[file_type_class]), file_type_class)
-            extractors[f'.{file_type}'] = extractor_class()
-        except AttributeError:
-            raise ValueError(f"Unsupported extractor class: {file_type_class}")
-    
-    return extractors
-
-def read_db(input_dir):
-    dbs = glob(f'{input_dir}**/*.db', recursive=True)
-    df_list = []
-    for db in dbs:
-        conn = sqlite3.connect(db)
-        cur = conn.cursor()
-        df_list.append(pd.read_sql_query('SELECT * FROM docs', conn))
-        conn.close()
-    final_df = pd.concat(df_list, ignore_index=True)
-    return final_df
