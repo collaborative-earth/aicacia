9becbc8fa5bfc4f4344e785761e5593381bdb12d
Vlad <grastasss@gmail.com>
2025-04-15 18:59:16 +0200

    Make adjustments 1

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-BRANCH=${1:-main}
-
-cd /home/ec2-user/aicacia
-
 POSTGRES_PASSWORD=$(aws ssm get-parameter --name "/aicacia-app/postgres-password" --with-decryption --query "Parameter.Value" --output text)
-
-echo "Checking out branch: $BRANCH"
-git fetch origin
-git checkout "$BRANCH"
-git pull origin "$BRANCH"
 
 echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" > ./api/.env
 
