5dda50ac431a17cb268fa33c44754d26fdf50c62
Vlad <grastasss@gmail.com>
2025-04-16 14:55:30 +0200

    Make adjustments 12

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -2,11 +2,28 @@
 
 set -e
 
+REGION="us-east-2"
+ACCOUNT_ID="771589226584"
+
+echo "Pulling secrets..."
 POSTGRES_PASSWORD=$(aws ssm get-parameter --name "/aicacia-app/postgres-password" --with-decryption --query "Parameter.Value" --output text)
 
 export POSTGRES_PASSWORD
 
-echo "Starting services..."
+echo "Logging in to ECR..."
+aws ecr get-login-password --region $REGION | \
+  docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
+echo "Pulling images..."
+docker pull postgres:17
+docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/aicacia-api:latest
+docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/aicacia-webapp:latest
+
+echo "Stopping services..."
 docker compose down
-docker compose pull aicacia-postgres
+
+echo "Starting services..."
 docker compose up -d
+
+echo "Cleaning up unused images"
+docker image prune -f
