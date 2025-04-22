e6b59c19ddb86bd75b4606642834602c2f708648
Vlad <grastasss@gmail.com>
2025-04-22 23:02:06 +0200

    Finish wri data upload script

--- data_ingest/sources/wri_data_upload.py
+++ data_ingest/sources/wri_data_upload.py
@@ -4,19 +4,36 @@ import urllib.request
 
 def upload_wri_pdfs_to_s3(pg_url, bucket_name):
     query = """
-        select distinct on (doc_id) doc_id, link 
+        select distinct on (doc_id) doc_id, id, link 
         from source_links 
         where type = 'application/pdf' 
         order by doc_id, updated_at
     """
+
+    s3_client = boto3.client("s3")
 
     with psycopg.connect(pg_url) as conn:
         with conn.cursor() as cur:
             cur.execute(query)
             links = cur.fetchall()
 
-            for doc_id, link in links:
-                print(doc_id, link)
+            for i, (doc_id, link_id, link) in enumerate(links):
+                print(f"Downloading and saving file: {link} ({i}/{len(links)})")
+
+                object_key = f'wri/{doc_id}.pdf'
+
+                try:
+                    urllib.request.urlretrieve(link, './temp.pdf')
+
+                    with open("./temp.pdf", "rb") as f:
+                        s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=f)
+                        cur.execute("update source_links set s3_location = %s where id = %s", (object_key, link_id,))
+                        conn.commit()
+
+                except Exception as error:
+                    print(f"Exception occurred for: {link}")
+                    print(error)
+
 
 if __name__ == '__main__':
-    upload_wri_pdfs_to_s3("postgresql://postgres:aicacia@3.137.35.87:5432/aicacia_db", "aicacia-extracted-data")
+    upload_wri_pdfs_to_s3("<DB_URL>", "aicacia-extracted-data")
