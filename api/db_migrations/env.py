c5efdacef47895c7b044ae23bf780da526a90139
Guru <guru@coefficient.io>
2025-06-19 08:16:17 -0700

    Adding dev docker-compose for easy local development

--- api/db_migrations/env.py
+++ api/db_migrations/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
 
 from alembic import context
+from server.db.models.base import Base
 from sqlalchemy import engine_from_config, pool
-
-from server import entities
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +17,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = entities.Base.metadata
+target_metadata = Base.metadata
 
 cmd_opts = context.get_x_argument(as_dictionary=True)
 
